0ab6f01d947fe9986d1e21ba6f18d22aabf7a758
amod-publisher <sharpazure0@gmail.com>
2023-11-04 17:55:12 +0000

    update debug layer constructor

--- os-devkit-v1/devkit_src/shared/client/ui/Layer.ts
+++ os-devkit-v1/devkit_src/shared/client/ui/Layer.ts
@@ -1,13 +1,15 @@
 import { Make } from "@rbxts/altmake";
+import { RunService } from "@rbxts/services";
 import { bs } from "shared/bs";
 
 // base Layer class implementation for non-user generated Layers
 
 export abstract class _InhLayer {
-    private Instance: bs.Layer;
+    public readonly Instance: bs.Layer;
 
     constructor(Parent?: ScreenGui) {
         this.Instance = Make("CanvasGroup", {
+            Visible: false,
             Parent: Parent
         });
     }
@@ -15,12 +17,63 @@ export abstract class _InhLayer {
 
 // extensions of _InhLayer for game reserved layers (debug, ...)
 
-export class DebugLayer extends _InhLayer {}
+export class DebugLayer extends _InhLayer {
+    constructor(Scale?: number) {
+        super();
+        
+        // give the layer instance the appropriate properties
+
+        Scale = Scale || 1;
+
+        this.Instance.AnchorPoint = new Vector2(0.5, 0.5);
+        
+        this.Instance.Position = UDim2.fromScale(0.5, 0.5);
+        this.Instance.Size = UDim2.fromScale(Scale, Scale);
+
+        // make the needed constraints
+
+        // padding of 0.01
+
+        Make("UIPadding", {
+            PaddingTop: new UDim(0.01),
+            PaddingLeft: new UDim(0.01),
+
+            Parent: this.Instance
+        });
+
+        // list for the items 
+
+        Make("UIListLayout", {
+            Wraps: true,
+            
+            FillDirection: Enum.FillDirection.Vertical,
+            ItemLineAlignment: Enum.ItemLineAlignment.Start,
+            
+            VerticalAlignment: Enum.VerticalAlignment.Top,
+            HorizontalAlignment: Enum.HorizontalAlignment.Left,
+
+            Padding: new UDim(0, 2),
+
+            HorizontalFlex: Enum.UIFlexAlignment.SpaceEvenly,
+
+            Parent: this.Instance
+        });
+
+        // create textlabels
+
+        const fps_handle = Make("TextLabel", {
+            Text: "fps (1/dt): //",
+            Parent: this.Instance
+        });
+
+        RunService.BindToRenderStep("debug_updater", Enum.RenderPriority.Camera.Value, (dt) => {
+            fps_handle.Text = "fps (1/dt): " + math.floor(1 / dt); 
+        })
+    }
+}
 export class ReservedLayer extends _InhLayer {}
 
 // user instanced layer
-
-// 
 
 export class InstanceableLayer {
     private Instance: bs.Layer;
